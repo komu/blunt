b05cc4be5e4e405c6f6d759372ec800f79024773
Juha Komulainen <juha.komulainen@evident.fi>
2011-12-06 18:15:36 +0200

    Refactoring.

--- src/main/java/fi/evident/dojolisp/eval/types/BasicType.java
+++ src/main/java/fi/evident/dojolisp/eval/types/BasicType.java
@@ -1,13 +1,37 @@
 package fi.evident.dojolisp.eval.types;
 
+import fi.evident.dojolisp.eval.AnalyzationException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static fi.evident.dojolisp.utils.Objects.requireNonNull;
 
 public final class BasicType extends Type {
     
     private final String name;
-    
+
+    private static final Map<String, BasicType> basicTypes = new HashMap<String, BasicType>();
+
     BasicType(String name) {
         this.name = requireNonNull(name);
+    }
+
+    static Type createOrGet(String name) {
+        BasicType type = basicTypes.get(name);
+        if (type == null) {
+            type = new BasicType(name);
+            basicTypes.put(name, type);
+        }
+        return type;
+    }
+    
+    public static Type get(String name) {
+        BasicType type = basicTypes.get(name);
+        if (type != null)
+            return type;
+        else
+            throw new AnalyzationException("unknown type: '" + name + "'");
     }
 
     @Override
--- src/main/java/fi/evident/dojolisp/eval/types/Type.java
+++ src/main/java/fi/evident/dojolisp/eval/types/Type.java
@@ -1,45 +1,26 @@
 package fi.evident.dojolisp.eval.types;
 
-import fi.evident.dojolisp.eval.AnalyzationException;
 import fi.evident.dojolisp.eval.TypeCheckException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public abstract class Type {
     
-    private static final Map<String, Type> basicTypes = new HashMap<String, Type>();
-
     Type() { }
     
-    public static final Type UNIT = basicType("Unit");
-    public static final Type INTEGER = basicType("Integer");
-    public static final Type BOOLEAN = basicType("Boolean");
-    public static final Type STRING = basicType("String");
-
-    private static Type basicType(String name) {
-        Type type = basicTypes.get(name);
-        if (type == null) {
-            type = new BasicType(name);
-            basicTypes.put(name, type);
-        }
-        return type;
-    }
-
-    public static Type forName(String name) {
-        Type type = basicTypes.get(name);
-        if (type != null)
-            return type;
-        else
-            throw new AnalyzationException("unknown type: '" + name + "'");
-    }
+    public static final Type UNIT = BasicType.createOrGet("Unit");
+    public static final Type INTEGER = BasicType.createOrGet("Integer");
+    public static final Type BOOLEAN = BasicType.createOrGet("Boolean");
+    public static final Type STRING = BasicType.createOrGet("String");
 
     public static Type fromClass(Class<?> type) {
         return (type == Void.class)                             ? UNIT
              : (type == Boolean.class || type == boolean.class) ? BOOLEAN
              : (type == Integer.class || type == int.class)     ? INTEGER
              : (type == String.class)                           ? STRING
-             : basicType(type.getName());
+             : BasicType.createOrGet(type.getName());
+    }
+    
+    public static Type forName(String name) {
+        return BasicType.get(name);
     }
 
     public Type unify(Type type) {
