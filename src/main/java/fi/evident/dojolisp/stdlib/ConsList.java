f3263e2950adf67aab8bb6f84a9f7c543ad28a95
Juha Komulainen <juha.komulainen@evident.fi>
2011-12-16 16:44:03 +0200

    Pretty-printing of ConsLists.

--- src/main/java/fi/evident/dojolisp/stdlib/ConsList.java
+++ src/main/java/fi/evident/dojolisp/stdlib/ConsList.java
@@ -1,31 +1,77 @@
 package fi.evident.dojolisp.stdlib;
 
-public final class ConsList<T> {
-    final T head;
-    final ConsList<T> tail;
+@SuppressWarnings("unused")
+public abstract class ConsList<T> {
     
-    private ConsList(T head, ConsList<T> tail) {
-        this.head = head;
-        this.tail = tail;
+    private static final class Cons<T> extends ConsList<T> {
+        final T head;
+        final ConsList<T> tail;
+        
+        private Cons(T head, ConsList<T> tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+
+        @Override
+        protected T getHead() {
+            return head;
+        }
+
+        @Override
+        protected ConsList<T> getTail() {
+            return tail;
+        }
+    }
+    
+    private static final class Nil<T> extends ConsList<T> {
+        @Override
+        protected T getHead() {
+            throw new UnsupportedOperationException("head of nil");
+        }
+
+        @Override
+        protected ConsList<T> getTail() {
+            throw new UnsupportedOperationException("tail of nil");
+        }
+    }
+    
+    protected abstract T getHead();
+    protected abstract ConsList<T> getTail();
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        
+        ConsList<T> item = this;
+        while (item instanceof Cons) {
+            Cons cons = (Cons) item;
+            sb.append(sb.length() == 0 ? "[" : ", ");
+            
+            sb.append(cons.head);
+            item = cons.tail;
+        }
+
+        sb.append("]");
+        return sb.toString();
     }
 
     @LibraryFunction("nil")
     public static <T> ConsList<T> nil() {
-        return null;
+        return new Nil<T>();
     }
 
     @LibraryFunction("cons")
     public static <T> ConsList<T> cons(T head, ConsList<T> tail) {
-        return new ConsList<T>(head, tail);
+        return new Cons<T>(head, tail);
     }
     
     @LibraryFunction("head")
     public static <T> T head(ConsList<T> list) {
-        return list.head;
+        return list.getHead();
     }
 
     @LibraryFunction("tail")
     public static <T> ConsList<T> tail(ConsList<T> list) {
-        return list.tail;
+        return list.getTail();
     }
 }
