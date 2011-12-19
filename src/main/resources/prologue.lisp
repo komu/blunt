43cdd3296f6966557cd4efdfe3384c922c8801e4
Juha Komulainen <juha.komulainen@evident.fi>
2011-12-19 11:13:36 +0200

    Added foldr, sum and product to prologue.

--- src/main/resources/prologue.lisp
+++ src/main/resources/prologue.lisp
@@ -1,3 +1,16 @@
+(define foldr
+  (lambda (f e l)
+    (if (nil? l)
+        e
+        (f (head l)
+           (foldr f e (tail l))))))
+
+(define sum
+  (lambda (l) (foldr + 0 l)))
+
+(define product
+  (lambda (l) (foldr * 1 l)))
+
 (define map
   (lambda (f l)
     (if (nil? l)
@@ -7,3 +20,6 @@
 
 (define square
   (lambda (x) (* x x)))
+
+(define numbers
+  (cons 1 (cons 2 (cons 3 (cons 4 (nil))))))
