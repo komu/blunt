38c62ab066389b5341c638b4e4ae9cbf6897def5
Juha Komulainen <juha.komulainen@evident.fi>
2012-02-15 20:57:47 +0200

    Ported DataTypeParser to Kotlin.

---
+++ src/main/kotlin/komu.blunt.parser/DataTypeParser.kt
@@ -0,0 +1,82 @@
+package komu.blunt.parser
+
+import com.google.common.collect.ImmutableList
+import komu.blunt.ast.AST
+import komu.blunt.ast.ASTDataDefinition
+import komu.blunt.types.*
+
+import java.util.ArrayList
+import java.util.List
+
+import com.google.common.base.Preconditions.checkNotNull
+import komu.blunt.parser.TokenType.*
+import komu.blunt.types.Qualified.quantify
+import komu.blunt.types.Type.functionType
+import komu.blunt.types.Type.genericType
+
+private class DataTypeParser(val lexer: Lexer, val typeParser: TypeParser) {
+
+    // data <type> <var>* = <constructor>+
+    fun parseDataDefinition(): ASTDataDefinition  {
+        lexer.expectIndentStartToken(DATA);
+
+        val builder = DataTypeBuilder(lexer.readTokenValue(TYPE_OR_CTOR_NAME).sure())
+
+        while (!lexer.readMatchingToken(ASSIGN))
+            builder.addVariable(typeParser.parseTypeVariable().sure())
+
+        do {
+            parseConstructor(builder)
+        } while (lexer.readMatchingToken(OR))
+
+        if (lexer.readMatchingToken(DERIVING)) {
+            lexer.expectToken(LPAREN)
+            do {
+                builder.addAutomaticallyDerivedClass(lexer.readTokenValue(TYPE_OR_CTOR_NAME).sure())
+            } while (lexer.readMatchingToken(COMMA))
+            lexer.expectToken(RPAREN)
+        }
+
+        lexer.expectToken(END)
+
+        return builder.build()
+    }
+
+    private fun parseConstructor(builder: DataTypeBuilder): Unit {
+        val constructorName = lexer.readTokenValue(TYPE_OR_CTOR_NAME).sure()
+
+        val args = ArrayList<Type?>()
+        while (!lexer.nextTokenIs(OR) && !lexer.nextTokenIs(END) && !lexer.nextTokenIs(DERIVING))
+            args.add(typeParser.parseTypePrimitive())
+
+        builder.addConstructor(constructorName, args)
+    }
+
+    private class DataTypeBuilder(val typeName: String) {
+
+        private val vars = ArrayList<TypeVariable?>();
+        private val constructors = ArrayList<ConstructorDefinition?>()
+        private val derivedClasses = ArrayList<String?>()
+        private var constructorIndex = 0
+
+        public fun addVariable(variable: TypeVariable) {
+            vars.add(variable)
+        }
+
+        public fun addConstructor(constructorName: String, args: List<Type?>) {
+            val scheme = quantify(vars, Qualified<Type?>(functionType(args, getType())))
+            constructors.add(ConstructorDefinition(constructorIndex++, constructorName, scheme, args.size()));
+        }
+
+        private fun getType() =
+            genericType(typeName, vars)
+
+        public fun addAutomaticallyDerivedClass(className: String) {
+            derivedClasses.add(className)
+        }
+
+        public fun build(): ASTDataDefinition =
+            AST.data(typeName, getType(), ImmutableList.copyOf(constructors), ImmutableList.copyOf(derivedClasses)).sure()
+    }
+}
+
