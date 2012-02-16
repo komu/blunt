dd004927b4990ed3765dd5ca8ef73b5b9e42c6c3
Juha Komulainen <juha.komulainen@evident.fi>
2012-02-16 21:30:23 +0200

    Ported IdentifierMapping to Kotlin.

---
+++ src/main/kotlin/komu/blunt/analyzer/IdentifierMapping.kt
@@ -0,0 +1,38 @@
+package komu.blunt.analyzer
+
+import komu.blunt.objects.Symbol
+
+import java.util.HashMap
+
+private class IdentifierMapping(val parent: IdentifierMapping?) {
+
+    private val mappings = HashMap<Symbol,Symbol>()
+
+    this(): this(null) { }
+
+    public fun get(v: Symbol): Symbol {
+        var mapping = this
+        while (true) {
+            val sym = mapping.mappings.get(v)
+            if (sym != null)
+                return sym
+
+            val parent = mapping.parent
+            if (parent == null)
+                break
+            else
+                mapping = parent
+        }
+
+        return v;
+    }
+
+    public fun put(oldName: Symbol, newName: Symbol) {
+        val old = mappings.put(oldName, newName)
+        if (old != null)
+            throw IllegalArgumentException("duplicate mapping for '$oldName'");
+    }
+
+    public fun extend(): IdentifierMapping =
+        IdentifierMapping(this)
+}
