74a7d52fb202099726e0861122279d75ed6c2461
Juha Komulainen <juha.komulainen@evident.fi>
2012-02-20 16:03:10 +0200

    Ported CoreLambdaExpression to Kotlin.

---
+++ src/main/kotlin/komu/blunt/core/CoreLambdaExpression.kt
@@ -0,0 +1,35 @@
+package komu.blunt.core
+
+import komu.blunt.asm.*
+
+import com.google.common.base.Preconditions.checkArgument
+
+class CoreLambdaExpression(private val envSize: Int, private val body: CoreExpression?) : CoreExpression() {
+
+    {
+        checkArgument(envSize >= 0)
+    }
+
+    override fun assemble(asm: Assembler?, target: Register?, linkage: Linkage?): Instructions {
+        val instructions = Instructions()
+
+        // TODO: place the lambda in a new code section
+        val lambda = asm?.newLabel("lambda")
+        val afterLambda = asm?.newLabel("after-lambda")
+
+        instructions.loadLambda(target, lambda)
+        if (linkage == Linkage.NEXT)
+            instructions.jump(afterLambda)
+        else
+            instructions.finishWithLinkage(linkage)
+
+        instructions.label(lambda)
+        instructions.createEnvironment(envSize)
+        instructions.append(body?.assemble(asm, Register.VAL, Linkage.RETURN))
+        instructions.label(afterLambda)
+
+        return instructions
+    }
+
+    override fun simplify() = CoreLambdaExpression(envSize, body?.simplify())
+}
