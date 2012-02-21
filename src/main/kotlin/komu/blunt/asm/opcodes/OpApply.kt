106c9513ba116a362f37601492c2a4c074808175
Juha Komulainen <juha.komulainen@evident.fi>
2012-02-21 10:03:36 +0200

    Ported OpApply and OpCreateEnvironment to Kotlin.

---
+++ src/main/kotlin/komu/blunt/asm/opcodes/OpApply.kt
@@ -0,0 +1,43 @@
+package komu.blunt.asm.opcodes
+
+import komu.blunt.asm.Register
+import komu.blunt.asm.VM
+import komu.blunt.objects.CompoundProcedure
+import komu.blunt.objects.PrimitiveProcedure
+import komu.blunt.objects.Procedure
+import komu.blunt.objects.EvaluationException
+
+class OpApply private(private val tail: Boolean) : OpCode() {
+
+    class object {
+        val NORMAL = OpApply(false)
+        val TAIL   = OpApply(true)
+    }
+
+    override fun execute(vm0: VM?) {
+        val vm = vm0.sure()
+        val procedure = vm.procedure
+
+        when (procedure) {
+            is PrimitiveProcedure -> executePrimitive(vm, procedure)
+            is CompoundProcedure  -> executeCompound(vm, procedure)
+            else                  -> throw EvaluationException("invalid proceduce '$procedure'")
+        }
+    }
+
+    private fun executePrimitive(vm: VM, procedure: PrimitiveProcedure) {
+        vm.`val` = procedure.apply(vm.arg)
+        vm.pc = vm.pop() as Int
+    }
+
+    private fun executeCompound(vm: VM, procedure: CompoundProcedure) {
+        vm.env = procedure.env
+        vm.pc = procedure.address
+    }
+
+    // Though the application itself will only modify PC, ENV or VAL, the called code could modify anything.
+    override fun modifies(register: Register?) = true
+
+    override fun toString() = "(${if (tail) "tail-call" else "call"} ${Register.PROCEDURE} ${Register.ARG}"
+}
+
---
+++ src/main/kotlin/komu/blunt/asm/opcodes/OpCreateEnvironment.kt
@@ -0,0 +1,21 @@
+package komu.blunt.asm.opcodes
+
+import komu.blunt.asm.Register
+import komu.blunt.asm.VM
+
+class OpCreateEnvironment(private val envSize: Int) : OpCode() {
+
+    {
+        if (envSize < 0) throw IllegalArgumentException("negative envSize: $envSize")
+    }
+
+    override fun execute(vm0: VM?) {
+        val vm = vm0.sure()
+        vm.env = vm.env?.extend(envSize, vm.arg)
+    }
+
+    override fun modifies(register: Register?) = register == Register.ENV
+
+    override fun toString() = "(load ${Register.ENV} (create-env ${Register.ENV} ${Register.ENV} $envSize))"
+}
+
