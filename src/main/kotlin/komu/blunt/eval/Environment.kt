9e901316c2ef43a73ae4a5148182c9da4fe16bac
Juha Komulainen <juha.komulainen@evident.fi>
2012-02-21 17:09:39 +0200

    Ported Environment to Kotlin.

--- src/main/kotlin/komu/blunt/eval/Environment.kt
+++ src/main/kotlin/komu/blunt/eval/Environment.kt
@@ -6,6 +6,29 @@ import java.util.Arrays
 
 import java.lang.Math.max
 
+abstract class Environment {
+
+    fun lookup(v: VariableReference) =
+        lookup(v.frame, v.offset)
+
+    fun set(v: VariableReference, value: Any?) {
+        set(v.frame, v.offset, value)
+    }
+
+    protected abstract fun lookup(frame: Int, offset: Int): Any?
+    protected abstract fun set(frame: Int, offset: Int, value: Any?)
+
+    fun extend(envSize: Int, arg: Any?): Environment {
+        val args = Array<Any?>(envSize)
+        args[0] = arg
+        return NestedEnvironment(args, this)
+    }
+
+    fun extend(envSize: Int): Environment {
+        return NestedEnvironment(Array<Any?>(envSize), this)
+    }
+}
+
 class RootEnvironment : Environment() {
 
     private var bindings = Array<Any?>(512)
@@ -34,5 +57,21 @@ class RootEnvironment : Environment() {
     private fun checkFrame(frame: Int) {
         if (frame != 0 && frame != VariableReference.GLOBAL_FRAME)
             throw IllegalArgumentException("invalid frame $frame");
+    }
+}
+
+class NestedEnvironment(private val bindings: Array<Any?>, private val parent: Environment) : Environment() {
+
+    override fun lookup(frame: Int, offset: Int): Any? =
+        if (frame == 0)
+            bindings[offset]
+        else
+            parent.lookup(frame-1, offset)
+
+    override fun set(frame: Int, offset: Int, value: Any?) {
+        if (frame == 0)
+            bindings[offset] = value
+        else
+            parent.set(frame-1, offset, value)
     }
 }
