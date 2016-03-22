0f514816396250df318f90aad6638e6ea497372f
Juha Komulainen <juha.komulainen@evident.fi>
2016-03-22 23:44:08 +0200

    Remove unnecessary quotes

--- src/main/kotlin/komu/blunt/types/Scheme.kt
+++ src/main/kotlin/komu/blunt/types/Scheme.kt
@@ -1,19 +1,19 @@
 package komu.blunt.types
 
-import java.util.Objects
 import komu.blunt.types.checker.Substitution
+import java.util.*
 
-class Scheme(val kinds: List<Kind>, val `type`: Qualified<Type>) : Types<Scheme> {
+class Scheme(val kinds: List<Kind>, val type: Qualified<Type>) : Types<Scheme> {
 
     override fun apply(substitution: Substitution): Scheme =
-        Scheme(kinds, `type`.apply(substitution))
+        Scheme(kinds, type.apply(substitution))
 
     override fun addTypeVariables(result: MutableSet<TypeVariable>) {
-        `type`.addTypeVariables(result)
+        type.addTypeVariables(result)
     }
 
-    override fun toString() = `type`.toString()
+    override fun toString() = type.toString()
 
-    override fun equals(other: Any?) = other is Scheme && kinds == other.kinds && `type` == other.`type`
-    override fun hashCode() = Objects.hash(kinds, `type`)
+    override fun equals(other: Any?) = other is Scheme && kinds == other.kinds && type == other.type
+    override fun hashCode() = Objects.hash(kinds, type)
 }
